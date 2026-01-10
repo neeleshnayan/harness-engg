0ea5de0c9afad34c2900680a6ac20b25fa6aa43c
Akshansh Bhanjana <akshansh2000@gmail.com>
2026-01-10 16:31:15 +0530

    remove unneeded reference

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,6 @@ import https from 'https';
 
 // Set the base URL for API requests
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
-const WEB3_API_BASE_URL = process.env.NEXT_PUBLIC_WEB3_API_URL || 'http://127.0.0.1:8080';
 const KRYPTON_WEB3_API_BASE_URL = process.env.NEXT_PUBLIC_KRYPTON_WEB3_API_URL || 'http://127.0.0.1:8001';
 
 const httpAgent = new http.Agent({
